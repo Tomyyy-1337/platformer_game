c98506143e5832b3dfddf14d02e568f74869b590
Tom Paßberg <passberg@rhrk.uni-kl.de>
2024-03-04 21:57:47 +0100

    input improvements

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use bevy::prelude::*;
-
+use bevy::{prelude::*, window::PresentMode};
 use bevy_rapier2d::prelude::*;
 
 mod camera;
@@ -9,8 +8,16 @@ mod state;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugins((
+            DefaultPlugins.set(ImagePlugin::default_nearest()).set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Platformer".into(),
+                    resolution: (1000., 750.).into(),
+                    // present_mode: PresentMode::Mailbox,
+                    ..default()
+                    }),
+                ..default()
+            }),
             RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0),
             RapierDebugRenderPlugin::default(),
             world::WorldPlugin,
